f74dcfbeec1523b8ff80c59646fc972462030382
mariaparfenyuk <umme0807@gmail.com>
2021-04-17 21:49:20 +0300

    models vue

---
+++ vue-crash-course/src/components/AddTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text">
+    <button type="submit">Create</button>
+  </form>
+</template>
+
+<script> 
+export default {
+  data(){
+  //  return 
+  // https://www.youtube.com/watch?v=OlnwgS-gk8Y 28:18
+  },
+  methods: {
+    onSubmit() {
+      console.log("Submit");
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+  input {
+    width: 400px;
+  }
+</style>
